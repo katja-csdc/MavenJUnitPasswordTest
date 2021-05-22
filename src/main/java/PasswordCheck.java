ffc05b89c8033375c9618926a5d74c165a4a3be0
katja-csdc <katja.sabadash@gmail.com>
2021-05-22 06:32:54 +0200

    Exercise_1_Try_1

---
+++ src/main/java/PasswordCheck.java
@@ -0,0 +1,42 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PasswordCheck {
+    public static boolean isValid(String password) {
+        if (password.length() < 8 || password.length() > 25)
+            return false;
+
+        String regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[()#$?!%/@]).+$";
+
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(password);
+
+        if (!m.matches())
+            return false;
+
+        int l = password.length();
+
+        for (int i = 0; i < l - 3; i++) {
+            if (Character.isDigit(password.charAt(i)) && Character.isDigit(password.charAt(i + 1)) &&
+                    Character.isDigit(password.charAt(i + 2)) && Character.isDigit(password.charAt(i + 3))) {
+                if (password.charAt(i) == password.charAt(i + 1) &&
+                        password.charAt(i + 1) == password.charAt(i + 2) &&
+                        password.charAt(i + 2) == password.charAt(i + 3)) {
+                    return false;
+                }
+            }
+        }
+
+        for (int i = 0; i < l - 2; i++) {
+            if (Character.isDigit(password.charAt(i)) && Character.isDigit(password.charAt(i + 1)) &&
+                    Character.isDigit(password.charAt(i + 2))){
+                if ((int)password.charAt(i) == ((int)password.charAt(i + 1) - 1) &&
+                        (int)password.charAt(i + 1) == ((int)password.charAt(i + 2) - 1)){
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/checkPasswordTest.java
@@ -0,0 +1,60 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import static org.junit.jupiter.api.Assertions.*;
+
+class isValidTest {
+
+    @Test
+    @DisplayName("Has password longer length")
+    void testPasswordLengthLonger() {
+        assertFalse(PasswordCheck.isValid("aaaaaaaaaaaaaaaaaaaaaaaaa"));
+    }
+
+    @Test
+    @DisplayName("Has password shorter length")
+    void testPasswordLengthShorter() {
+        assertFalse(PasswordCheck.isValid("aaaas"));
+    }
+
+    @Test
+    @DisplayName("Has password no small letters")
+    void testPasswordForSmallLetters() {
+        assertFalse(PasswordCheck.isValid("QWEASDZXC12#3"));
+    }
+
+    @Test
+    @DisplayName("Has password no capital letters")
+    void testPasswordForCapitalLetters() {
+        assertFalse(PasswordCheck.isValid("qweasdzxc12#3"));
+    }
+
+    @Test
+    @DisplayName("Has password no numbers")
+    void testPasswordForNumbers() {
+        assertFalse(PasswordCheck.isValid("QWEASDzxc##"));
+    }
+
+    @Test
+    @DisplayName("Has password no special characters")
+    void testPasswordFoSpecialChars() {
+        assertFalse(PasswordCheck.isValid("QWEASDzxc132"));
+    }
+
+    @Test
+    @DisplayName("Has password consecutive numbers")
+    void checkForConsecutiveNumbers() {
+        assertFalse(PasswordCheck.isValid("QWEASDzxc123#"));
+    }
+
+    @Test
+    @DisplayName("Has password same 4 numbers")
+    void checkForSameNumbers() {
+        assertFalse(PasswordCheck.isValid("QWEASDzxc1111#"));
+    }
+
+    @Test
+    @DisplayName("Is password correct")
+    void checkForCorrectPassword() {
+        assertTrue(PasswordCheck.isValid("QWEASDzxc12#3"));
+    }
+}
